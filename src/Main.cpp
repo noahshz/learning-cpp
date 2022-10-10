14034b81ce54587321dd620ba7f30d542b339218
Noah <borkitube@gmail.com>
2022-10-10 09:42:01 +0200

    sda

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,28 +11,6 @@ using namespace std;
 const double PI = 3.141;
 
 int main(int argc, char** argv) {
-	cout << "Hello World :)" << endl;
-
-	// data types
-	short int i1 = 1;			// -32768 - +32767
-	int i2 = 50000;			// -2147483648 - +2147483647
-	long long int i3 = 17886669669;	// -9223372036854775808 - +9223372036854775807
-	cout << "minimum Integer: " << numeric_limits<int>::min() << endl;
-	cout << "maximum Integer: " << numeric_limits<int>::max() << endl;
-
-	float f1 = 1.2;				// 1.17549e-38 - 3.40282e+38
-
-	double d1 = 3.50;			// 2.22507e-308 - 1.79769e+308
-	long double d2 = 2.1;		// 3.621e-4932 - 1.18973e+4932
-
-	char grade = 'A';
-	string name = "Noah";
-
-	bool alive = true;
-
-	auto whatAmI = "test";
-
-
 	// user input
 	int num1, num2;
 	cout << "Enter a Number: ";
