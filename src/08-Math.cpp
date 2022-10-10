dc1f47d925fd2137894c903efb8cc1c97c641ca3
Noah <borkitube@gmail.com>
2022-10-10 10:59:39 +0200

    math

---
+++ src/08-Math.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    cout << "abs(-10) = " << abs(-10) << endl;
+    cout << "max(5,4) = " << max(5,4) << endl;
+    cout << "min(5,4) = " << min(5,4) << endl;
+    cout << "fmax(5.3, 4.3) = " << fmax(5.3, 4.3) << endl;
+    cout << "fmin(5.3, 4.3) = " << fmin(5.3, 4.3) << endl;
+    cout << "ceil(10.45) = " << ceil(10.45) << endl;
+    cout << "floor(10.45) = " << floor(10.45) << endl;
+    cout << "round(10.45) = " << round(10.45) << endl;
+    cout << "pow(2, 3) = " << pow(2, 3) << endl; // 2³
+    cout << "sqrt(100) = " << sqrt(100) << endl; // squareroot
+    cout << "cbrt(100) = " << cbrt(100) << endl; // cuberoot
+
+
+
+
+    string a;
+    cin >> a;
+    return 0;
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -4,30 +4,10 @@
 #include <sstream>
 #include <ctime>
 #include <limits>
-#include "Car.cpp"
 
 using namespace std;
 
-const double PI = 3.141;
-
 int main() {
-	Car bmw = Car("BMW", "Schwarz", 143);
-	cout << "Marke: " << bmw.getMarke() << endl;
-	cout << "Farbe: " << bmw.getFarbe() << endl;
-	cout << "PS: " << to_string(bmw.getPS()) << endl;
-	cout << "Neuwagen: " << (bmw.isNew() ? "Ja" : "Nein") << endl;
-	cout << "Kilometerstand: " << bmw.getKM() << endl;
-
-	bmw.drive();
-	bmw.drive();
-	bmw.drive();
-
-	cout << "Neuwagen: " << (bmw.isNew() ? "Ja" : "Nein") << endl;
-	cout << "Kilometerstand: " << bmw.getKM() << endl;
 	
-	string t;
-	cout << "Input to end application..";
-	cin >> t;
-
 	return 0;
 }
