31ab866f8c7f9609ba77ac119dab7440f6f342b5
Noah <borkitube@gmail.com>
2022-10-10 09:57:04 +0200

    Update DataTypes.cpp

--- src/DataTypes.cpp
+++ src/DataTypes.cpp
@@ -3,16 +3,16 @@
 
 int main() {
     // Integers
-    short int i_short = 100;		        // -32768 - +32767
-	int i_int = 5111340;			        // -2147483648 - +2147483647
-	long long int i_long = 17886669669;	    // -9223372036854775808 - +9223372036854775807
+    short int i_short = 100;    // -32768 - +32767
+	int i_int = 5111340;    // -2147483648 - +2147483647
+	long long int i_long = 17886669669; // -9223372036854775808 - +9223372036854775807
 
     // Floats
-    float f_short = 0.1;                    // 1.17549e-38 - 3.40282e+38
+    float f_short = 0.1;    // 1.17549e-38 - 3.40282e+38
 
     // Doubles
-    double d_double = 3.50;			// 2.22507e-308 - 1.79769e+308
-	long double d_long = 2.1;		// 3.621e-4932 - 1.18973e+4932
+    double d_double = 3.50; // 2.22507e-308 - 1.79769e+308
+	long double d_long = 2.1;   // 3.621e-4932 - 1.18973e+4932
 
     // Char & String
     char c = 'N';
