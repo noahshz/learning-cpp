9537e4b624d3eba85ec3dc9134ac42a0bba61dad
Noah <borkitube@gmail.com>
2022-10-10 09:21:14 +0200

    test class

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <ctime>
 #include <limits>
+#include "Car.cpp"
 
 using namespace std;
 
@@ -139,6 +140,20 @@ int main(int argc, char** argv) {
 	string bsp = "test";
 
 	cout << "Address of Variable bsp: " << &bsp << endl;
+
+	Car bmw = Car("BMW", "Schwarz", 143);
+	cout << "Marke: " << bmw.getMarke() << endl;
+	cout << "Farbe: " << bmw.getFarbe() << endl;
+	cout << "PS: " << to_string(bmw.getPS()) << endl;
+	cout << "Neuwagen: " << (bmw.isNew() ? "Ja" : "Nein") << endl;
+	cout << "Kilometerstand: " << bmw.getKM() << endl;
+
+	bmw.drive();
+	bmw.drive();
+	bmw.drive();
+
+	cout << "Neuwagen: " << (bmw.isNew() ? "Ja" : "Nein") << endl;
+	cout << "Kilometerstand: " << bmw.getKM() << endl;
 	
 	string t;
 	cout << "Input to end application..";
