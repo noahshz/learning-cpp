73734c7a041db477500e561d745996366e4258aa
Noah <borkitube@gmail.com>
2022-10-24 10:17:35 +0200

    Update 15-Containers.cpp

--- src/15-Containers.cpp
+++ src/15-Containers.cpp
@@ -10,22 +10,19 @@
 #include <ctime>
 #include <cmath>
 #include <deque>
+#include <iterator>
 
 using namespace std;
 
-void awaitInput();
-
 int main() {
+    // vector
+    vector<int> v = {1, 2, 3, 4};
+    v.push_back(5);
+    for(unsigned int i : v) cout << i << endl;
+
     // deque
     deque<int> d = { 1, 2, 3, 4 };
     d.push_front(0);
     d.push_back(5);
     for(unsigned int i : d) cout << i << endl;
-
-    awaitInput();
-}
-
-void awaitInput() {
-    string input;
-    cin >> input;
 }
